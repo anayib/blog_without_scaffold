6e1bdb0c1779261e4e9f5b03a01f7e4e2bb0b2d0
anayib <nayib.abdala@gmail.com>
2014-12-04 17:14:37 -0500

    creates update, edit js.erb files , adds <id=<%=post.id%> to modal to link update.js.erb ith jquery method html to replace the curret date in the row

--- SuperBlog/app/controllers/posts_controller.rb
+++ SuperBlog/app/controllers/posts_controller.rb
@@ -17,18 +17,36 @@ class PostsController < ApplicationController
     
     @post = Post.new
 
+     #al crear el archivo new.js.erb el render que el 
+    #controlador hace por defecto lo va a hacer con 
+    #formato js porque le puse en el link_to del index.htm
+    #el metodo remote: true, es decir hágalo en formato js
+
   end
 
   def edit
     @post = Post.find(params[:id])
+    #al crear el archivo edit.js.erb el render que el 
+    #controlador hace por defecto lo va a hacer con 
+    #formato js porque le puse en el link_to del index.htm
+    #el metodo remote: true, es decir hágalo en formato js
   end
 
   def create
     @post = Post.create(post_params)
+    #al crear el archivo create.js.erb el render que el 
+    #controlador hace por defecto lo va a hacer con 
+    #formato js porque le puse en el link_to del index.htm
+    #el metodo remote: true, es decir hágalo en formato js
   end
 
   def update
     @post = Post.update(params[:id], post_params)
+    #al crear el archivo update.js.erb el render que el 
+    #controlador hace por defecto lo va a hacer con 
+    #formato js porque le puse en el link_to del index.htm
+    #el metodo remote: true, es decir hágalo en formato js
+
   end
 
   def destroy
@@ -37,7 +55,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'El post fue destruido exitosamente' }
       format.json { head :no_content }
-      
+
     end
   end
 
